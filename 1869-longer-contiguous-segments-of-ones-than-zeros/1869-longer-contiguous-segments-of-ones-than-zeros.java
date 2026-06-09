b789879ac98a1c838df1d761cbf44750525491b2
ABINANDU R S <abinandu2005@gmail.com>
2026-06-09 12:27:36 +0530

    Time: 1 ms (75.78%), Space: 42.9 MB (50.78%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.java
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.java
@@ -1,26 +1,20 @@
 class Solution {
     public boolean checkZeroOnes(String s) {
-        int nums[]=new int[s.length()];
-        for(int i=0;i<s.length();i++){
-            nums[i]=s.charAt(i)-'0';
-        } 
-        int cnt1=0;
-        int cnt0=0;
-        int max0=0,max1=0;
-        for(int i=0;i<=nums.length-1;i++){
-            if(nums[i]==1){
+        int max0 = 0, max1 = 0;
+        int cnt0 = 0, cnt1 = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1') {
                 cnt1++;
-            }
-            else cnt1=0;
-            if(cnt1>max1)max1=cnt1;
-            if(nums[i]==0){
+                cnt0 = 0;
+                max1 = Math.max(max1, cnt1);
+            } else {
                 cnt0++;
+                cnt1 = 0;
+                max0 = Math.max(max0, cnt0);
             }
-            else cnt0=0;
-            if(cnt0>max0)max0=cnt0;
         }
-        
-        if(max0>=max1)return false;
-        return true;
+
+        return max1 > max0;
     }
 }
