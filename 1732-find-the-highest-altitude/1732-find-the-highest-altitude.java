52a2b8a65cd70727bd2b2082a81e118890c5d2ad
ABINANDU R S <abinandu2005@gmail.com>
2026-06-20 09:24:43 +0530

    Time: 74 ms (1.26%), Space: 43 MB (55.66%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max=0;
+        int a=0;
+        for(int i=0;i<gain.length;i++){
+            a=gain[i]+a;
+            if(a>max){
+                max=a;
+            }
+        }
+        return max;
+    }
+}
