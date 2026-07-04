4248e6477a7e86ae6128f956475cfba9fd9f19f5
ABINANDU R S <abinandu2005@gmail.com>
2026-07-04 09:47:18 +0530

    Time: 0 ms (100%), Space: 42.4 MB (16.18%) - LeetHub

---
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int getMaximumGenerated(int n) {
+        int max=1;
+        int a[]=new int[n+1];
+        if(n==0)return 0;
+        a[0]=0;
+        a[1]=1;
+        int j=1;
+        int k=1;
+        int l=2;
+        for(int i=2;i<a.length;i++){
+            if(i%2==0){
+                a[i]=a[j];
+                j++;
+            }
+            else{
+                a[i]=a[k]+a[l];
+                k++;
+                l++;
+            }
+            max=Math.max(max,a[i]);
+        }
+        return max;
+    }
+}
