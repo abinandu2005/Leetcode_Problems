4044213f2def8c02eba5267291fc013ee052a292
ABINANDU R S <abinandu2005@gmail.com>
2026-07-14 20:19:54 +0530

    Time: 31 ms (56.05%), Space: 48.4 MB (83.83%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        Set <Integer> set=new HashSet<>();
+        for(int i:candyType){
+            set.add(i);
+        }
+        if(set.size()>candyType.length/2)return candyType.length/2;
+        return set.size();
+    }
+}
