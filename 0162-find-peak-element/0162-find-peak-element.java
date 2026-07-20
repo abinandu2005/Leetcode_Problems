3b125e5297ad677731ec97189aa1c2347b5c30f9
ABINANDU R S <abinandu2005@gmail.com>
2026-07-20 23:32:49 +0530

    Time: 0 ms (100%), Space: 43.9 MB (94.5%) - LeetHub

---
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max=Integer.MIN_VALUE;
+        int ind=0;
+        for(int i=0;i<nums.length;i++){
+            if(max<nums[i]){
+                max=nums[i];
+                ind=i;
+            }
+        }
+
+        return ind;
+    }
+}
