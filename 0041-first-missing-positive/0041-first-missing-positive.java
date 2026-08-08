2d6738ad6f12f46fcef935d4b8f8dcc6c40f80a0
ABINANDU R S <abinandu2005@gmail.com>
2026-08-08 09:53:44 +0530

    Time: 17 ms (18.01%), Space: 73.9 MB (25.83%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        int idx=1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==idx)idx++;
+            else if(nums[i]>idx)return idx;
+        }
+        return idx;
+    }
+}
